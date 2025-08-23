3720f31edec22664da4613c8feae2cdbe08796ea
buseiry <buseiry.habeeb.olayinka@lasustech.edu.ng>
2025-08-23 08:16:12 +0100

    my_list

---
+++ OneDrive/Desktop/python assignment 2/my_list.py
@@ -0,0 +1,21 @@
+# 1. create empty list []
+#append the following element[10,20,30,40]
+my_list =[10,20,30,40]
+# insert the value 15 at the second position 
+my_list.insert(1,15)
+# Extend my_list with another list : [50, 60, 70].
+my_list.extend([50,60,70])
+#  Remove the last element from my_list.
+my_list.pop()
+# Sort my_list in ascending order.
+my_list.sort()
+# Find and print the index of the value 30 in my_list.
+
+
+
+
+
+print (my_list.index(30))
+
+
+
